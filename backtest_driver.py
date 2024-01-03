137f495f0777a46f0b47f6929cde8e8ca3fa25b5
Irv Shapiro <ishapiro@cogitations.com>
2024-01-02 18:30:42 -0600

    Added ability to process multiple configuration files

--- backtest_driver.py
+++ backtest_driver.py
@@ -41,64 +41,65 @@ strategy_parameters = {
 # Get a list of all files in the current directory
 files = os.listdir("strategy_configurations/")
 
-# Look for configurations files in the strategy configuration directory
+# Loop through all of the configurations files in the strategy configuration directory
+# Then load the parameters and run the strategy backtest
+
 for toml_file in files:
     # Check if the file is a TOML file
     if toml_file.endswith('.toml'):
         strategy_file = toml_file
         print(f"Strategy file found: {strategy_file}")
-        break
 
-# Read parameters from a TOML file
-strategy_parameters = toml.load(f"strategy_configurations/{strategy_file}")
-print()
-print("**************************************************")
-print("Strategy Parameters read from TOML file")
-pp.pprint(strategy_parameters)
-print("**************************************************")
-print()
+        # Read parameters from a TOML file
+        strategy_parameters = toml.load(f"strategy_configurations/{strategy_file}")
+        print()
+        print("**************************************************")
+        print("Strategy Parameters read from TOML file")
+        pp.pprint(strategy_parameters)
+        print("**************************************************")
+        print()
 
-capital_budget =  strategy_parameters["distance_of_wings"] \
-    * 100 * strategy_parameters["quantity_to_trade"] \
-    * strategy_parameters["margin_call_factor"]
+        capital_budget =  strategy_parameters["distance_of_wings"] \
+            * 100 * strategy_parameters["quantity_to_trade"] \
+            * strategy_parameters["margin_call_factor"]
 
-backtesting_start = datetime.combine(strategy_parameters["starting_date"], datetime.min.time())
-backtesting_end = datetime.combine(strategy_parameters["ending_date"], datetime.min.time())
+        backtesting_start = datetime.combine(strategy_parameters["starting_date"], datetime.min.time())
+        backtesting_end = datetime.combine(strategy_parameters["ending_date"], datetime.min.time())
 
-# Override the parameters set in the OptionsIronCondorMWT class
-OptionsIronCondorMWT.set_parameters(strategy_parameters)
+        # Override the parameters set in the OptionsIronCondorMWT class
+        OptionsIronCondorMWT.set_parameters(strategy_parameters)
 
-strategy_name = f'ic-{strategy_parameters["symbol"]}-{strategy_parameters["delta_required"]}delta-{strategy_parameters["option_duration"]}dur-{strategy_parameters["days_before_expiry_to_buy_back"]}ex-{strategy_parameters["minimum_hold_period"]}hd'
+        strategy_name = f'ic-{strategy_parameters["symbol"]}-{strategy_parameters["delta_required"]}delta-{strategy_parameters["option_duration"]}dur-{strategy_parameters["days_before_expiry_to_buy_back"]}ex-{strategy_parameters["minimum_hold_period"]}hd'
 
-trading_fee = TradingFee(percent_fee=strategy_parameters["trading_fee_percent"])  # Account for trading fees and slipage
+        trading_fee = TradingFee(percent_fee=strategy_parameters["trading_fee_percent"])  # Account for trading fees and slipage
 
-# Execute the strategy with the current parameters
+        # Execute the strategy with the current parameters
 
-OptionsIronCondorMWT.backtest(
-    PolygonDataBacktesting,
-    backtesting_start,
-    backtesting_end,
-    benchmark_asset=strategy_parameters["symbol"],
-    buy_trading_fees=[trading_fee],
-    sell_trading_fees=[trading_fee],
-    polygon_api_key=POLYGON_CONFIG["API_KEY"],
-    polygon_has_paid_subscription=True,
-    name=strategy_name,
-    budget = capital_budget,
-)
+        OptionsIronCondorMWT.backtest(
+            PolygonDataBacktesting,
+            backtesting_start,
+            backtesting_end,
+            benchmark_asset=strategy_parameters["symbol"],
+            buy_trading_fees=[trading_fee],
+            sell_trading_fees=[trading_fee],
+            polygon_api_key=POLYGON_CONFIG["API_KEY"],
+            polygon_has_paid_subscription=True,
+            name=strategy_name,
+            budget = capital_budget,
+        )
 
-source_dir = "logs/"
-strategy_directory = strategy_file.split(".")[0]    
-target_dir = f"strategy_logs/{strategy_directory}/"
+        source_dir = "logs/"
+        strategy_directory = strategy_file.split(".")[0]    
+        target_dir = f"strategy_logs/{strategy_directory}/"
 
-# Create the target directory if it does not exist
-os.makedirs(target_dir, exist_ok=True)
+        # Create the target directory if it does not exist
+        os.makedirs(target_dir, exist_ok=True)
 
-# Get a list of all files in Lumibot log directory
-files = os.listdir(source_dir)
+        # Get a list of all files in Lumibot log directory
+        files = os.listdir(source_dir)
 
-# Copy each file to the strategy log directory
-# Leave in the original log directory so the browser can display it
-for file in files:
-    shutil.copy(os.path.join(source_dir, file), target_dir)
+        # Copy each file to the strategy log directory
+        # Leave in the original log directory so the browser can display it
+        for file in files:
+            shutil.copy(os.path.join(source_dir, file), target_dir)
 
